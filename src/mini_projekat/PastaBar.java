d5cc18341dc01605b1879905be97cbccc3fe1c14
Jelena Nicic <nicicj9@gmail.com>
2022-01-11 14:44:40 +0100

    Ispravka

--- src/mini_projekat/PastaBar.java
+++ src/mini_projekat/PastaBar.java
@@ -5,64 +5,27 @@ import java.util.Scanner;
 public class PastaBar {
 
 	public static void main(String[] args) {
-		
+
 		Scanner s = new Scanner(System.in);
 
-		String[] nizStalneMusterije = new String[6];
-		String[] nizSastojci = new String[16];
-		;
-		int[] nizCene = new int[16];
-
-		nizStalneMusterije[0] = "0631111111";
-		nizStalneMusterije[1] = "063222222";
-		nizStalneMusterije[2] = "063333333";
-		nizStalneMusterije[3] = "064444444";
-		nizStalneMusterije[4] = "065555555";
-		nizStalneMusterije[5] = "066666666";
-
-		nizSastojci[0] = "Makarone";
-		nizCene[0] = 50;
-		nizSastojci[1] = "Spagete";
-		nizCene[1] = 60;
-		nizSastojci[2] = "Bolognese";
-		nizCene[2] = 120;
-		nizSastojci[3] = "Curetina";
-		nizCene[3] = 120;
-		nizSastojci[4] = "Govedja prsuta";
-		nizCene[4] = 140;
-		nizSastojci[5] = "Slanina";
-		nizCene[5] = 100;
-		nizSastojci[6] = "Piletina";
-		nizCene[6] = 100;
-		nizSastojci[7] = "4 sira";
-		nizCene[7] = 100;
-		nizSastojci[8] = "Dimljeni sir";
-		nizCene[8] = 80;
-		nizSastojci[9] = "Parmezan";
-		nizCene[9] = 50;
-		nizSastojci[10] = "Pavlaka";
-		nizCene[10] = 80;
-		nizSastojci[11] = "Pesto sos";
-		nizCene[11] = 80;
-		nizSastojci[12] = "Napolitana";
-		nizCene[12] = 80;
-		nizSastojci[13] = "Povrce mix";
-		nizCene[13] = 50;
-		nizSastojci[14] = "Pecurke";
-		nizCene[14] = 50;
-		nizSastojci[15] = "Kutija";
-		nizCene[15] = 20;
+		String[] nizStalneMusterije = { "0631111111", "063222222", "063333333", "064444444", "065555555", "066666666" };
+		String[] nizSastojci = { "Makarone", "Spagete", "Bolognes", "Curetina", "Govedja prsuta", "Slanina", "Piletina",
+				"4 sira", "Dimljeni sir", "Parmezan", "Pavlaka", "Pesto sos", "Napolitana", "Povrce mix", "Pecurke",
+				"Kutija" };
+		int[] nizCene = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
 
 		double ukupnaCena = 0;
 		int i = 0;
 
 		System.out.println("Izvolite!!");
 
-		while (i >= 0) {
+		while (i != -2) {
 			System.out.println("Unesite sastojak: ");
 			i = nadjiIndeksSastojka(nizSastojci, s.next());
 			if (i >= 0) {
 				ukupnaCena = ukupnaCena + nizCene[i];
+			} else if (i != -2) {
+				System.out.println("Ne postoji trazeni sastojak");
 			}
 		}
 
@@ -81,7 +44,6 @@ public class PastaBar {
 	}
 
 	public static int nadjiIndeksSastojka(String[] nizSastojaka, String naziv) {
-
 		for (int i = 0; i < nizSastojaka.length; i++) {
 
 			if (nizSastojaka[i].equals(naziv)) {
@@ -89,7 +51,9 @@ public class PastaBar {
 				return i;
 			}
 		}
-
+		if (naziv.equals("Poruci")) {
+			return -2;
+		}
 		return -1;
 	}
 
